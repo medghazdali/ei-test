c17cc3815aecc6d80d5822b88254f39660e2f840
med <med.ghazdali@gmail.com>
2021-02-24 21:02:02 +0100

    comit base

--- base.sql
+++ base.sql
@@ -1,30 +1,112 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.7
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:8889
+-- Généré le : mer. 24 fév. 2021 à 20:01
+-- Version du serveur :  5.7.32
+-- Version de PHP : 7.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Base de données : `base`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `group`
+--
+
 CREATE TABLE `group` (
-  `id_group` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id_group`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  `id_group` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `group` VALUES (1,'Groupe 1'),(2,'Groupe 2'),(3,'Groupe 3');
+--
+-- Déchargement des données de la table `group`
+--
 
+INSERT INTO `group` (`id_group`, `name`) VALUES
+(1, 'Groupe 1'),
+(2, 'Groupe 2'),
+(3, 'Groupe 3');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
 
 CREATE TABLE `user` (
-  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
   `username` varchar(45) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `last_login` datetime DEFAULT NULL,
-  `status` int(11) DEFAULT NULL,
-  
-  PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `user` VALUES (1,'test','test','2011-09-28 12:00:00');
+--
+-- Déchargement des données de la table `user`
+--
 
+INSERT INTO `user` (`id_user`, `username`, `password`, `last_login`, `status`) VALUES
+(1, 'test', '$2y$10$PzYOvPzYN6nej8WoJRuSlOsiUHUTJAYaD5PfDtNVlFVSUAmuZZrv6', '2021-02-24 19:58:15', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_group`
+--
 
 CREATE TABLE `user_group` (
   `id_user` int(11) NOT NULL,
-  `id_group` int(11) NOT NULL,
-  PRIMARY KEY (`id_user`,`id_group`)
+  `id_group` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `user_group` VALUES (1,2);
+--
+-- Déchargement des données de la table `user_group`
+--
 
+INSERT INTO `user_group` (`id_user`, `id_group`) VALUES
+(1, 2);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `group`
+--
+ALTER TABLE `group`
+  ADD PRIMARY KEY (`id_group`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- Index pour la table `user_group`
+--
+ALTER TABLE `user_group`
+  ADD PRIMARY KEY (`id_user`,`id_group`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `group`
+--
+ALTER TABLE `group`
+  MODIFY `id_group` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
